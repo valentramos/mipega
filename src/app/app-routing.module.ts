60efd00c238abcb51dd556c9f6c5ee804c7aa0f7
valentramos <ignacio1994.ir@gmail.com>
2021-03-01 21:41:55 -0400

    comingsoon-page added and build prod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ComingsoonComponent } from './pages/comingsoon/comingsoon.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+  // { path: '', pathMatch: 'full' },
+  // { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  // { path: '', component: ComingsoonComponent },
+  // { path: 'inicio', component: AppComponent }
+  // { path: 'pagenotfound', component: PagenotfoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { ComingsoonComponent } from './pages/comingsoon/comingsoon.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagenotfoundComponent,
+    ComingsoonComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
