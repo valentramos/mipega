68b1d66408d533796a38179e464115f600fdb4e8
valentramos <ignacio1994.ir@gmail.com>
2021-03-15 11:35:18 -0400

    build prod and base-href configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { LandingpageComponent } from './pages/landingpage/landingpage.component'
 import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  // { path: '', pathMatch: 'full' },
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '', pathMatch: 'full', component: LandingpageComponent },
+  // { path: '', redirectTo: '/inicio', pathMatch: 'full' },
   // { path: '', component: ComingsoonComponent },
-  { path: 'inicio', component: LandingpageComponent }
+  // { path: 'inicio', component: LandingpageComponent }
   // { path: 'pagenotfound', component: PagenotfoundComponent },
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
